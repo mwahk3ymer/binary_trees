baef253ef4e9e451437c078651acdb9be4b53893
Mwahk3ymer <audreymwakima99@gmail.com>
2023-11-02 12:03:27 +0000

    ./18-sibling

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -12,14 +12,11 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 
 	binary_tree_t *grandparent = node->parent->parent;
 
-	if (grandparent->left == node->parent)
+	if (grandparent->left && grandparent->right)
 	{
-		if (grandparent->right)
+		if (grandparent->left == node->parent)
 			return (grandparent->right);
-	}
-	else if (grandparent->right == node->parent)
-	{
-		if (grandparent->left)
+		else
 			return (grandparent->left);
 	}
 
